1cfd6aa23f51b50b5a1af3c87a7fb9220758ea4c
Lucieno Zandry <josulandrie0@gmail.com>
2024-08-22 16:10:13 +0300

    added configuration for static files

---
+++ declaration.d.ts
@@ -0,0 +1,62 @@
+// Image files
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
+
+// Audio files
+declare module '*.mp3' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.wav' {
+  const value: string;
+  export default value;
+}
+
+// Font files
+declare module '*.woff' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.woff2' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ttf' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.otf' {
+  const value: string;
+  export default value;
+}
